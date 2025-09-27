3c92d4f565371d056582f74d632a193033395336
Shubham11git <khobragadeshubham55@gmail.com>
2025-09-27 18:27:51 +0530

    updated the file

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ elevenlabs = ElevenLabs(
   api_key=os.getenv("ELEVENLABS_API_KEY"),
 )
 
+voice_id = os.getenv("VOICE_ID")
+
 st.title("Text to Speech App")
 
 text = st.text_area("Enter text to convert to speech:")
@@ -19,18 +21,15 @@ if st.button("Convert to Speech"):
     if text:
         audio = elevenlabs.text_to_speech.convert(
             text=text,
-            voice_id="JBFqnCBsd6RMkjVDRZzb",
+            voice_id=voice_id,
             model_id="eleven_multilingual_v2",
             output_format="mp3_44100_128",
         )
 
-        # audio_bytes = io.BytesIO(audio)
-        # play(audio)
-
-         # Join chunks into one byte string
+        # Combine the list of byte chunks into a single byte string
         audio_data = b"".join(audio)
 
-        # Wrap in buffer
+        # Create a BytesIO object from the byte string
         audio_bytes = io.BytesIO(audio_data)
 
         st.audio(audio_bytes, format='audio/mp3', autoplay=True)
